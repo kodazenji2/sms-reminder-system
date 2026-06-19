4aa6976bae2cbb64569aa0080a00dcef33bfb33a
kodazenji2 <futurevibez598@gmail.com>
2026-06-19 01:43:27 +0100

    Update NICTMBrand.tsx

--- components/ui/NICTMBrand.tsx
+++ components/ui/NICTMBrand.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export function NICTMBrand({ inverted = false }: { inverted?: boolean }) {
   return (
     <Image
-      src="/nictm_logo.jpg"
+      src="/NICTM LOGO.jpg"
       alt="NICTM Logo"
       width={96}
       height={96}
